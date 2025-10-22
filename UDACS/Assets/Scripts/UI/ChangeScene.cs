1a95b11d78cf5bbff747a8aa149034140207562f
AzrielMakesStuff <azrieldied@gmail.com>
2025-10-22 17:18:09 +1300

    Menus navigable and settings are persistent

---
+++ UDACS/Assets/Scripts/UI/ChangeScene.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadScene(string sceneName) => SceneManager.LoadScene(sceneName);
+}
--- UDACS/Assets/Scripts/UI/UIManager.cs
+++ UDACS/Assets/Scripts/UI/UIManager.cs
@@ -3,45 +3,118 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class UIManager: MonoBehaviour
 {
+    public bool isGameScene;
+
+    [Header("Required")]
+    public Toggle pauseWhilePausedToggle;
+    public Toggle infiniteAmmoToggle;
+    public Toggle invertRightStickToggle;
+    public Toggle dayCycleToggle;
+    public Slider snapCameraAngleSlider;
+    public Slider deadzoneSlider;
+    public Slider volumeSlider;
+
+    DroneController droneController;
+    ThrowObject payloadSystem;
+
+    [Header("Game Scene Only")]
     public PausePlay pausePlayScript;
     public DayNight dayNightScript;
     public GameObject droneObject;
     public ThrowObject loadingSystem;
     public GameObject grenadeUI;
-
-    DroneController droneController;
-    ThrowObject payloadSystem;
     void Start()
     {
-        droneController = droneObject.GetComponent<DroneController>();
-        payloadSystem = droneController.GetComponent<ThrowObject>();
+        if (isGameScene)
+        {
+            droneController = droneObject.GetComponent<DroneController>();
+            payloadSystem = droneController.GetComponent<ThrowObject>();
+        }
+        SetToggles();
+        SetSettings();
     }
-    void Update() => grenadeUI.SetActive(loadingSystem.loaded);
+    void SetToggles()
+    {
+        pauseWhilePausedToggle.isOn = Settings.PauseWhilePaused;
+        infiniteAmmoToggle.isOn = Settings.InfiniteAmmo;
+        invertRightStickToggle.isOn = Settings.InvertRightStick;
+        dayCycleToggle.isOn = Settings.DayCycle;
+
+        snapCameraAngleSlider.value = Settings.SnapCameraAngle;
+        deadzoneSlider.value = Settings.Deadzone;
+        volumeSlider.value = Settings.Volume;
+    }
+    void SetSettings()
+    {
+        if (isGameScene)
+        {
+            pausePlayScript.pauseWhenOpen = Settings.PauseWhilePaused;
+            payloadSystem.requiresLoading = Settings.InfiniteAmmo;
+            loadingSystem.loaded = Settings.InfiniteAmmo;
+            droneController.invertRightStick = Settings.InvertRightStick;
+            dayNightScript.dayLength = Settings.DayCycle ? DayLength.Overdrive : DayLength.None;
+
+            droneController.upwardCameraPosition = Settings.SnapCameraAngle;
+            droneController.deadzone = Settings.Deadzone;
+            Audio.instance.mixer.SetFloat("MasterVolume", Settings.Volume - 80f);
+        }
+
+        snapCameraAngleSlider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{Settings.SnapCameraAngle:0}";
+        deadzoneSlider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{Settings.Deadzone:0.0}";
+        volumeSlider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{Settings.Volume:0}";
+    }
+    void Update()
+    {
+        if (isGameScene) grenadeUI.SetActive(loadingSystem.loaded);
+    }
     public void ExitGame() => Application.Quit();
     public void Respawn()
     {
         Time.timeScale = 1;
         SceneManager.LoadScene("TerrainScene");
     }
-    public void MainMenu() => SceneManager.LoadScene("Main Menu");
-    public void PauseWhilePaused(Toggle toggle) => pausePlayScript.pauseWhenOpen = toggle.isOn;
-    public void InfiniteAmmo(Toggle toggle) => payloadSystem.requiresLoading = toggle.isOn;
-    public void InvertRightStick(Toggle toggle) => droneController.invertRightStick = toggle.isOn;
-    public void DayCycle(Toggle toggle) => dayNightScript.dayLength = toggle.isOn ? DayLength.Overdrive : DayLength.None;
+    public void MainMenu() => SceneManager.LoadScene("Main Menu Scene");
+    public void PauseWhilePaused(Toggle toggle)
+    {
+        Settings.PauseWhilePaused = toggle.isOn;
+        if (isGameScene) pausePlayScript.pauseWhenOpen = toggle.isOn;
+    }
+    public void InfiniteAmmo(Toggle toggle)
+    {
+        Settings.InfiniteAmmo = toggle.isOn;
+        if (isGameScene)
+        {
+            payloadSystem.requiresLoading = toggle.isOn;
+            loadingSystem.loaded = toggle.isOn;
+        }
+    }
+    public void InvertRightStick(Toggle toggle)
+    {
+        Settings.InvertRightStick = toggle.isOn;
+        if (isGameScene) droneController.invertRightStick = toggle.isOn;
+    }
+    public void DayCycle(Toggle toggle)
+    {
+        Settings.DayCycle = toggle.isOn;
+        if (isGameScene) dayNightScript.dayLength = toggle.isOn ? DayLength.Overdrive : DayLength.None;
+    }
     public void SnapCameraAngle(Slider slider)
     {
-        droneController.upwardCameraPosition = slider.value;
-        slider.GetComponentsInChildren<Text>()[1].text = $"{slider.value:0}";
+        Settings.SnapCameraAngle = (byte)slider.value;
+        if (isGameScene) droneController.upwardCameraPosition = slider.value;
+        slider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{slider.value:0}";
     }
     public void Deadzone(Slider slider)
     {
-        droneController.deadzone = slider.value;
-        slider.GetComponentsInChildren<Text>()[1].text = $"{slider.value:0.0}";
+        Settings.Deadzone = (byte)slider.value;
+        if (isGameScene) droneController.deadzone = slider.value;
+        slider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{slider.value:0.0}";
 
     }
     public void Volume(Slider slider)
     {
-        Audio.instance.mixer.SetFloat("MasterVolume", slider.value - 80f);
-        slider.GetComponentsInChildren<Text>()[1].text = $"{slider.value:0}";
+        Settings.Volume = (byte)slider.value;
+        if (isGameScene) Audio.instance.mixer.SetFloat("MasterVolume", slider.value - 80f);
+        slider.GetComponentsInChildren<Text>()[isGameScene ? 1 : 0].text = $"{slider.value:0}";
     }
 }
